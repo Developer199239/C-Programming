040b34acecd26958161c1a7ee57ef0cd6b10cf5d
Jalilur Rahman Murtuza <murtuza.rahman83@gmail.com>
2019-02-04 18:57:42 +0600

    Hamiltonian and Lagrangian solve

--- Hackerearth/Data Structures/1-D/Hamiltonian_and_Lagrangian/Hamiltonian_and_Lagrangian/main.cpp
+++ Hackerearth/Data Structures/1-D/Hamiltonian_and_Lagrangian/Hamiltonian_and_Lagrangian/main.cpp
@@ -11,30 +11,37 @@
 #include <map>
 #include <string.h>
 #include <stdio.h>
+#include <set>
 using namespace std;
 int main(int argc, const char * argv[]) {
     int n;
     cin>>n;
     int minValue = 10001;
-    int arr[n],tempArr[n];
+    int arr[n];
+    set<int>myset;
     for(int i = 0; i<n; i++) {
         cin>>arr[i];
         if(arr[i] < minValue) {
             minValue = arr[i];
         }
     }
-    int j = 0;
-    tempArr[j++] = minValue;
+    
+    int tempArra[n],j = 0;
+    
     for(int i = n-1; i>=0;i--) {
         if(arr[i] > minValue) {
             minValue = arr[i];
-            tempArr[j++] = minValue;
+            tempArra[j++] = minValue;
+        } else if(arr[i] == minValue) {
+           tempArra[j++] = minValue;
         }
     }
     
+    
     for(int i = j - 1; i>=0; i--) {
-        cout<<tempArr[i]<<" ";
+        cout<<tempArra[i]<<" ";
     }
+    
     
     return 0;
 }
